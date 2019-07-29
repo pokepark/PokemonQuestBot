1a5dfd056b16b81dbe5a737155a4ffdd1bea284a
Chefkeks <Chefkeks@users.noreply.github.com>
2019-07-29 10:56:27 +0200

    Added INVASION_DURATION_EVENT and improved invasion creation.

--- mods/invasion_create.php
+++ mods/invasion_create.php
@@ -12,9 +12,6 @@ bot_access_check($update, 'invasion-create');
 // Pokestop id.
 $pokestop_id = $data['id'];
 
-// Approx. time until invasion disappears
-$invasion_time = $data['arg'];
-
 // Check if invasion already exists for this pokestop.
 $invasion_in_db = invasion_duplication_check($pokestop_id);
 
@@ -22,66 +19,54 @@ $invasion_in_db = invasion_duplication_check($pokestop_id);
 $keys = [];
 
 // Invasion already in database or new
-if(!$invasion_in_db && $invasion_time == 0) {
+if(!$invasion_in_db) {
     // Build message string.
     $msg = '';
     $stop = get_pokestop($pokestop_id, false);
     $msg .= getTranslation('pokestop') . ': <b>' . $stop['pokestop_name'] . '</b>' . (!empty($stop['address']) ? (CR . $stop['address']) : '');
     $msg .= CR . CR . '<b>' . getTranslation('invasion_select_time') . '</b>';
-    // Create the keys.
-    $keys = [
-        [
+
+    // Invasion event to short in 5 minute steps as duration
+    if(INVASION_DURATION_EVENT > INVASION_DURATION_LONG) { 
+        $slotmax = INVASION_DURATION_EVENT;
+        $slotsize = 5;
+   
+        // Create the keys.
+        for ($i = $slotmax; $i >= INVASION_DURATION_SHORT; $i = $i - $slotsize) {
+            $keys[] = array(
+                // Just show the time, no text - not everyone has a phone or tablet with a large screen...
+                'text'          => floor($i / 60) . ':' . str_pad($i % 60, 2, '0', STR_PAD_LEFT),
+                'callback_data' => $pokestop_id . ':invasion_save:' . $i
+            );
+        }
+
+        $keys = inline_key_array($keys, 5);
+        $keys = universal_key($keys, '0', 'exit', '0', getTranslation('abort'));
+        //$keys = array_merge($keys,$keys_exit);
+
+    // Duration short and long as selection
+    } else {
+        // Create the keys.
+        $keys = [
             [
-                'text'          => INVASION_DURATION_SHORT . 'min',
-                'callback_data' => $pokestop_id . ':invasion_create:' . INVASION_DURATION_SHORT
+                [
+                    'text'          => INVASION_DURATION_SHORT . 'min',
+                    'callback_data' => $pokestop_id . ':invasion_save:' . INVASION_DURATION_SHORT
+                ],
+                [
+                    'text'          => INVASION_DURATION_LONG . 'min',
+                    'callback_data' => $pokestop_id . ':invasion_save:' . INVASION_DURATION_LONG
+                ]
             ],
             [
-                'text'          => INVASION_DURATION_LONG . 'min',
-                'callback_data' => $pokestop_id . ':invasion_create:' . INVASION_DURATION_LONG
+                [
+                    'text'          => getTranslation('abort'),
+                    'callback_data' => '0:exit:0'
+                ]
             ]
-        ],
-        [
-            [
-                'text'          => getTranslation('abort'),
-                'callback_data' => '0:exit:0'
-            ]
-        ]
-    ];
+        ];
+    }
 
-// Save invasion or not?
-} else if (!$invasion_in_db && $invasion_time > 0) {
-    // Build message string.
-    $msg = '';
-    $stop = get_pokestop($pokestop_id, false);
-    $msg .= getTranslation('pokestop') . ': <b>' . $stop['pokestop_name'] . '</b>' . (!empty($stop['address']) ? (CR . $stop['address']) : '');
-    $msg .= CR . CR . '<b>' . getTranslation('invasion_expected_time') . SP . $data['arg'] . 'min' . '</b>';
-    $msg .= CR . CR . '<b>' . getTranslation('add_this_invasion') . '</b>';
-
-    // Create the keys.
-    $keys = [
-        [
-            [
-                'text'          => getTranslation('yes'),
-                'callback_data' => $pokestop_id . ':invasion_save:' . $invasion_time
-            ]
-        ],
-        [
-            [
-                'text'          => getTranslation('no'),
-                'callback_data' => '0:exit:0'
-            ]
-        ],
-        [
-            [
-                'text'          => getTranslation('back'),
-                'callback_data' => $pokestop_id . ':invasion_create:0'
-            ],
-            [
-                'text'          => getTranslation('abort'),
-                'callback_data' => '0:exit:0'
-            ]
-        ]
-    ];
 } else {
     // Invasion already in the database for this pokestop.
     $msg = EMOJI_WARN . '<b> ' . getTranslation('invasion_already_submitted') . ' </b>' . EMOJI_WARN . CR . CR;
